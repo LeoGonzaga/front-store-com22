836ceb2ce834570487aef55cb6c74cf1b5767e54
LeoGonzaga <sacredlotus05@gmail.com>
2020-10-20 23:27:40 -0300

    refatorando a tela inicial e deixando apenas como um select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Product } from './models/product';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'front-com222';
+export class AppComponent implements OnInit {
+  product = {} as Product;
+  products: Product[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {}
 }
